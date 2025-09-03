f937263a989c4c72d30ce411603b64c50e38f827
Reim-developer <contact.kaxtr@gmail.com>
2025-09-03 22:57:27 +0700

    implemented new core widget for embed builder slash command conetext.

---
+++ widgets/embed_builder_widget.py
@@ -0,0 +1,251 @@
+from dataclasses import dataclass, fields
+from discord import (
+	Interaction, TextStyle, Embed, ButtonStyle,
+	InteractionMessage
+)
+from discord.ext import commands
+from discord.ui import Modal, TextInput, View, button, Button
+from discord.ui.text_input import V
+from typing import (
+	Callable, List, Optional, 
+	Self, Tuple, TypeAlias
+)
+from re import match as regex_match
+from core_utils.type_alias import CanSendMessageChannel
+
+_T_INPUT: TypeAlias = TextInput[V]
+_T_S: TypeAlias = TextStyle
+_E: TypeAlias = Embed
+_WARNING_RESULT = Tuple[List[str], int]
+_ERR_RESULT = Tuple[List[str], int]
+_CHECKS: TypeAlias = List[Tuple[bool, Callable[[], str]]]
+_LIMIT_LENGTH: int = 6000
+_CSMC = CanSendMessageChannel
+
+def _is_valid_hex(hex: str) -> bool:
+	pattern = r"^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$"
+
+	return bool(regex_match(pattern = pattern, string = hex))
+
+def _fmt_color(hex: str) -> Optional[int]:
+	_BASE_INDEX = 16
+	if not _is_valid_hex(hex = hex):
+		return None
+	
+	if hex.startswith("#"):
+		return int(hex[1:], _BASE_INDEX)
+	
+	return int(hex, _BASE_INDEX)
+	
+@dataclass
+class _EmbedData:
+	title: Optional[str]
+	description: str
+	color: Optional[int]
+	footer_text: Optional[str]
+	author_text: Optional[str]
+
+class _BuilderOptions(View):
+	_b = button
+	_B_S = ButtonStyle
+	_E = _EmbedData
+
+	def __init__(
+			self, embed_data: _E,
+			message: InteractionMessage,
+			bot: commands.Bot) -> None:
+		super().__init__()
+		self.embed_data = embed_data
+		self.message = message
+		self.bot = bot
+
+	@_b(
+		label = "Send this embed to current channel",
+		style = _B_S.primary,
+	)
+	async def on_send(
+			self, interaction: Interaction,
+			_: Button[Self]) -> None:
+		
+		current_channel = interaction.channel
+		if isinstance(current_channel, _CSMC):
+			title = self.embed_data.title
+			description = self.embed_data.description
+			color = self.embed_data.color
+			footer_text = self.embed_data.footer_text
+			author_text = self.embed_data.author_text
+
+			await current_channel.send(
+				embed = (
+					Embed(
+						title = title,
+						description = description,
+						color = color
+					).set_footer(text = footer_text)
+					.set_author(name = author_text)
+				)
+			)
+
+			await interaction.response.send_message(
+				content = (
+					f"Successfully send message to: {current_channel.mention}!"
+				),
+				ephemeral = True
+			)
+			await self.message.delete()
+			
+class EmbedBuilderWidget(Modal):
+	title_build: _T_INPUT[Self] = TextInput(
+		label = "Embed title (Optional)",
+		style = _T_S.short,
+		placeholder = "...",
+		max_length = 256,
+		required = False
+	)
+	description_build: _T_INPUT[Self] = TextInput(
+		label = "Embed Description (Required)",
+		style = _T_S.paragraph,
+		placeholder = "...",
+		min_length = 1,
+		max_length = 4000,
+		required = True
+	)
+	color_build: _T_INPUT[Self] = TextInput(
+		label = "Embed HEX Color (Optional)",
+		placeholder = "...",
+		style = _T_S.short,
+		max_length = 20,
+		required = False
+	)
+	footer_text_build: _T_INPUT[Self] = TextInput(
+		label = "Embed Footer (Optional)",
+		placeholder = "...",
+		style = _T_S.paragraph,
+		max_length = 2048,
+		required = False
+	)
+	author_text_build: _T_INPUT[Self] = TextInput(
+		label = "Embed Author (Optional)",
+		placeholder = "...",
+		style = _T_S.short,
+		max_length = 256,
+		required = False
+	)
+
+	def __init__(self, bot: commands.Bot) -> None:
+		super().__init__(title = "Setup Embed")
+		self.bot = bot
+
+	def __generate_warnings(self, embed_data: _EmbedData) -> _WARNING_RESULT:
+		warnings_list = [
+			f"* **Embed field `{field.name}` is not provided/or not valid.**"
+			for field in fields(embed_data)
+			if not getattr(embed_data, field.name)
+		]
+
+		return warnings_list, len(warnings_list)
+	
+	def __generate_errors(self) -> _ERR_RESULT:
+		TITLE_LENGTH       = len(self.title_build.value)
+		DESCRIPTION_LENGTH = len(self.description_build.value)
+		FOOTER_TEXT_LENGTH = len(self.footer_text_build.value)
+		AUTHOR_TEXT_LENGTH = len(self.author_text_build.value)
+
+		checks: _CHECKS = [
+			(
+				TITLE_LENGTH +
+				DESCRIPTION_LENGTH +
+				AUTHOR_TEXT_LENGTH +
+				FOOTER_TEXT_LENGTH > _LIMIT_LENGTH,
+				lambda: (
+					"* **Your embed cannot be sent because it " \
+					f"exceeds Discord's {_LIMIT_LENGTH} character limit.**\n" \
+					"* **Traceback:**\n" 
+					f"* Title Length: `{TITLE_LENGTH}` character(s)\n" \
+					f"* Description Length: `{DESCRIPTION_LENGTH}` characters(s)\n" \
+					f"* Footer Text Length: `{FOOTER_TEXT_LENGTH}` characters(s)"
+				)
+			),
+			(
+				bool(self.color_build.value and not _is_valid_hex(self.color_build.value)),
+				lambda: (
+					"* Embed color does not exists, it only accepts HEX color format\n" \
+					f"* Your embed color is: `{self.color_build.value}`"
+				)
+			),
+		]
+
+		errors = [msg_fn() for condition, msg_fn in checks if condition]
+		return errors, len(errors)
+
+	async def on_submit(self, interaction: Interaction) -> None:
+		title: Optional[str] = (
+			self.title_build.value 
+			if self.title_build.value 
+			else None
+		)
+		color: Optional[int] = _fmt_color(self.color_build.value)
+		description: str = self.description_build.value
+		footer_text: Optional[str] = (
+			self.footer_text_build.value 
+			if self.footer_text_build.value 
+			else None
+		)
+		author_text: Optional[str] = (
+			self.author_text_build.value
+			if self.author_text_build.value
+			else None
+		)
+
+		embed_data = _EmbedData(
+			title = title,
+			description = description,
+			color = color,
+			footer_text = footer_text,
+			author_text = author_text
+		)
+		
+		warnings_reason, warning_count = self.__generate_warnings(
+			embed_data = embed_data
+		)
+		error_msg, err_count = self.__generate_errors()
+		error = "\n".join(error_msg)
+		if err_count:
+			await interaction.response.send_message(
+				content = error,
+				ephemeral = True
+			)
+			return
+
+		warn = "\n".join(warnings_reason)
+
+		await interaction.response.send_message(
+			content = (
+				"**__Information:__**\n" \
+				f"{(
+					"No warnings generated\n" 
+					if warning_count <= 0 
+					else
+					f"* {warning_count} warnings generated\n" \
+					"**Warnings Reason:**\n" \
+					f"{warn}\n\n" \
+					"* No errors generated\n\n"
+					if err_count <= 0
+					else "" # Auto-handling in otherwise case.
+				)}" \
+				"**__Preview:__**\n"
+			),
+			embed = _E(
+				title = title if title else None,
+				description = description,
+				color = color if color else None,
+			).set_footer(text = footer_text if footer_text else None),
+			ephemeral = True,
+		)
+
+		interaction_message = await interaction.original_response()
+		await interaction.edit_original_response(view = _BuilderOptions(
+			embed_data = embed_data,
+			message = interaction_message,
+			bot = self.bot
+		))
