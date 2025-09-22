64338dd78bc19c84a54042eda88354b94ecff602
Reim-developer <contact.kaxtr@gmail.com>
2025-09-23 04:22:01 +0700

    export `container_instance`

--- core_utils/__init__.py
+++ core_utils/__init__.py
@@ -3,9 +3,11 @@ from .giveaway_timer import GiveawayData, GiveawayTimer, TimerData
 from .type_alias import CanSendMessageChannel
 from .url import URLUtils
 from .logging import Log, StatusCode
+from .container import container_instance
 
 __all__ = [
 	"TimeUtils", "GiveawayData", 
 	"GiveawayTimer", "TimerData",
-	"CanSendMessageChannel", "URLUtils", "Log", "StatusCode"
+	"CanSendMessageChannel", "URLUtils", "Log", "StatusCode",
+	"container_instance"
 ]
