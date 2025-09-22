94be3824296c410292a3b27805353f4dbdb5eef7
Reim-developer <contact.kaxtr@gmail.com>
2025-09-23 04:22:13 +0700

    export `PostgresManager`

--- sql/__init__.py
+++ sql/__init__.py
@@ -1,3 +1,6 @@
 from .sql_manager import SQLiteManager
+from .postgres_manager import PostgresManager
+from .confession_manager import ConfessionManager
 
-__all__ = ["SQLiteManager"]
+__all__ = ["SQLiteManager", "PostgresManager", 
+		   "ConfessionManager"]
