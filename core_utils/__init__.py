8698dcf48d7013df5c3378a3eb538a048aef6a85
Reim-developer <contact.kaxtr@gmail.com>
2025-09-23 01:03:22 +0700

    Export `Log` class for log information with `debug`, `info`, `critical`, and exit program with error `fatal` Export enum class `StatusCode`

--- core_utils/__init__.py
+++ core_utils/__init__.py
@@ -2,9 +2,10 @@ from .time_utils import TimeUtils
 from .giveaway_timer import GiveawayData, GiveawayTimer, TimerData
 from .type_alias import CanSendMessageChannel
 from .url import URLUtils
+from .logging import Log, StatusCode
 
 __all__ = [
 	"TimeUtils", "GiveawayData", 
 	"GiveawayTimer", "TimerData",
-	"CanSendMessageChannel", "URLUtils"
+	"CanSendMessageChannel", "URLUtils", "Log", "StatusCode"
 ]
