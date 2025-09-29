c0f3fdcf726a749f4cc4fd9cce6b0852f9513d18
Reim-developer <contact.kaxtr@gmail.com>
2025-09-29 19:05:13 +0700

    Added `tables` Python module for constant table name of database

---
+++ sql/tables.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+class Tables(StrEnum):
+	GIVEAWAYS 		  = "giveaways"
+	BLACKLIST 		  = "blacklist"
+	GUILD_CONFIGS     = "guild_configs" 
+	DONATION_SETTINGS = "donation_settings"
+	DONATION_LOGS     = "donation_logs"
+	DONATION_TIERS    = "donation_tiers"
