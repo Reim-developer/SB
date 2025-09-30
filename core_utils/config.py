c3882101e10568aabd5ae5b7e05457eef56480ad
Reim-developer <contact.kaxtr@gmail.com>
2025-09-30 23:25:10 +0700

    Add gemini_api_keys method to ConfigUtils for loading Gemini API keys from config

--- core_utils/config.py
+++ core_utils/config.py
@@ -15,6 +15,25 @@ class ConfigUtils:
 	def __init__(self) -> None: ...
 
 	@staticmethod
+	def gemini_api_keys() -> str:
+		config_file = Path("config/config.toml")
+
+		if not config_file.exists():
+			Log.critical(f"TOML config file path not found: {config_file}")
+			Log.fatal(StatusCode.READ_CONFIG_ERR)
+
+		try:
+			with open(file = "config/config.toml", mode = "rb") as toml_cfg:
+				toml_data = load(toml_cfg)
+
+		except Exception as error:
+			Log.critical(f"Could not read or parse TOML string with error: {error}")
+			Log.fatal(StatusCode.DATABASE_CONNECT_ERR)
+
+		return toml_data["api"]["gemini_ai"]
+			
+
+	@staticmethod
 	def database_config() -> DatabaseConfig:
 		config_file = Path("config/config.toml")
 
