2d95cdf544ac04b15c5f566c649bbe1b8b51af99
Reim-developer <contact.kaxtr@gmail.com>
2025-09-30 23:24:58 +0700

    Add GoogleGeminiAPI to the API exports

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 from .my_anime_list import MyAnimeListApi
 from .types import MyAnimeList
+from .google_gemini import GoogleGeminiAPI
 
-__all__ = ["MyAnimeListApi", "MyAnimeList"]
+__all__ = ["MyAnimeListApi", "MyAnimeList", "GoogleGeminiAPI"]
