6aa0e0a41b8ab12a674ee93ba22ed1e8bdc6b63a
Reim-developer <contact.kaxtr@gmail.com>
2025-10-02 02:12:00 +0700

    defined new table `GUILDS`

--- sql/tables.py
+++ sql/tables.py
@@ -3,6 +3,7 @@ from enum import StrEnum
 class Tables(StrEnum):
 	GIVEAWAYS 		  = "giveaways"
 	BLACKLIST 		  = "blacklist"
+	GUILDS 			  = "guilds"
 	GUILD_CONFIGS     = "guild_configs" 
 	DONATION_SETTINGS = "donation_settings"
 	DONATION_LOGS     = "donation_logs"
