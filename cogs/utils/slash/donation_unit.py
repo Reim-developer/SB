ea67cd8965911ba33c47c137d2c37df5fe74a59a
Reim-developer <contact.kaxtr@gmail.com>
2025-10-03 01:11:26 +0700

    refactor: update donation unit handling to improve structure and enhance logging

--- cogs/utils/slash/donation_unit.py
+++ cogs/utils/slash/donation_unit.py
@@ -1,13 +1,12 @@
 from dataclasses import dataclass
 from discord.ext import commands
-from discord import app_commands, Interaction, Embed
+from discord import TextChannel, Thread, app_commands, Interaction, Embed
 from core_utils.container import container_instance
 from sql.donation_manager import DonationManager
-from core_utils.type_alias import CanSendMessageChannel
 from datetime import datetime 
 from core_utils.time_utils import TimeUtils
+from caches.donation_cache import DonationCache
 
-_CSMC = CanSendMessageChannel
 
 @dataclass
 class _UnitModifyContext:
@@ -16,20 +15,22 @@ class _UnitModifyContext:
 	money_unit:  str
 
 class DonationUnitSlash(commands.Cog):
-	_MIN_UNIT_LENGTH = 10
+	_MIN_UNIT_LENGTH = 32
 
 	def __init__(self, bot: commands.Bot) -> None:
 		self.bot  = bot
 		self.pool = container_instance.get_postgres_manager().pool
+		self.redis 			= container_instance.get_redis_manager().new_or_get()
+		self.donation_cache = DonationCache(redis = self.redis)
 
 		assert self.pool is not None
-		self.donation_manager = DonationManager(self.pool)
+		self.donation_manager = DonationManager(self.pool, self.donation_cache)
 
 	async def __send_log_unit_modify(self, ctx: _UnitModifyContext) -> None:
 		assert ctx.interaction.guild is not None # Interaction.guild will always not `None` here
 
 		channel = ctx.interaction.guild.get_channel(ctx.channel_id)
-		if channel and isinstance(channel, _CSMC):
+		if channel and isinstance(channel, TextChannel | Thread):
 			await channel.send(
 				embed = Embed(
 					title       = "Server Money Unit Modify",
@@ -47,20 +48,20 @@ class DonationUnitSlash(commands.Cog):
 		name = "donation_unit_set", 
 		description = "Set money unit of donation system")
 	@app_commands.describe(
-		money_unit = "Money unit of donation"
+		unit_name = "Money unit of donation"
 	)
 	@app_commands.checks.cooldown(rate = 1, per = 15, key = lambda i: i.user.id)
 	@app_commands.checks.bot_has_permissions(administrator = True)
 	@app_commands.default_permissions(administrator = True)
 	async def donation_unit_set(self, 
-							 interaction: Interaction, money_unit: str) -> None:
+							 interaction: Interaction, unit_name: str) -> None:
 		if not interaction.guild:
 			await interaction.response.send_message(
 				"Could not use this command in DM"
 			)
 			return 
 		
-		if len(money_unit) >= self._MIN_UNIT_LENGTH:
+		if len(unit_name) >= self._MIN_UNIT_LENGTH:
 			await interaction.response.send_message(
 				f"Could not set money unit over {self._MIN_UNIT_LENGTH} characters",
 				ephemeral = True
@@ -69,9 +70,9 @@ class DonationUnitSlash(commands.Cog):
 		
 		await interaction.response.defer()
 
-		await self.donation_manager.set_money_unit(
+		await self.donation_manager.add_unit_name(
 			guild_id   = interaction.guild.id,
-			money_unit = money_unit
+			unit_name  = unit_name
 		)
 
 		channel_id = await self.donation_manager.get_log_channel(
@@ -81,14 +82,15 @@ class DonationUnitSlash(commands.Cog):
 			await self.__send_log_unit_modify(_UnitModifyContext(
 				channel_id  = channel_id,
 				interaction = interaction,
-				money_unit  = money_unit
+				money_unit  = unit_name
 			))
 
 		await interaction.followup.send(
 			embed = (Embed(
 					title       = "Success",
 					description = (
-						f"Successfully setup money unit donation of server to: {money_unit}"
+						f"Successfully set unit name of donation for the "
+						f"server to: {unit_name}"
 					),
 					color 	  = 0xf4f5c9,
 					timestamp = datetime.now() 
