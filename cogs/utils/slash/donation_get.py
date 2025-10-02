4e5b1231cbf1ffbe5ffa59f724156764461a862c
Reim-developer <contact.kaxtr@gmail.com>
2025-10-03 01:11:09 +0700

    feat: enhance my_donation command to include unit name and improve donation retrieval

--- cogs/utils/slash/donation_get.py
+++ cogs/utils/slash/donation_get.py
@@ -1,24 +1,33 @@
-from discord.ext import commands
-from core_utils.container import container_instance
-from sql.donation_manager import DonationManager
-from discord import app_commands, Interaction, Embed
-from datetime import datetime
-from core_utils.number_fmt import Number
+from discord.ext                   import commands
+from core_utils.container          import container_instance
+from sql.donation_manager          import DonationManager
+from discord                       import app_commands, Interaction, Embed
+from datetime 			   		   import datetime
+from core_utils.number_fmt         import Number
+from caches.donation_cache 		   import DonationCache
+from core_utils.autocomplete_slash import AutoComplete
 
 class DonationGetSlash(commands.Cog):
 	def __init__(self, bot: commands.Bot) -> None:
 		self.bot  = bot
 		self.pool = container_instance.get_postgres_manager().pool
+		self.redis 			= container_instance.get_redis_manager().new_or_get()
+		self.donation_cache = DonationCache(redis = self.redis)
 
 		assert self.pool is not None
-		self.donation_manager = DonationManager(self.pool)
+		self.donation_manager = DonationManager(self.pool, self.donation_cache)
+		self.auto_complete    = AutoComplete(self.donation_manager)
+		self.my_donation.autocomplete("unit_name")(self.auto_complete.unit_autocomplete)
 
 	@app_commands.command(
 		name        = "my_donation",
 		description = "Show your donation"
 	)
+	@app_commands.describe(
+		unit_name = "Name of unit you want to show",
+	)
 	@app_commands.checks.bot_has_permissions(administrator = True)
-	async def my_donation(self, interaction: Interaction) -> None:
+	async def my_donation(self, interaction: Interaction, unit_name: str) -> None:
 		if not interaction.guild:
 			await interaction.response.send_message(
 				"Could not use this command in DM"
@@ -28,17 +37,15 @@ class DonationGetSlash(commands.Cog):
 		await interaction.response.defer()
 
 		money = await self.donation_manager.get_user_donation(
-			guild_id = interaction.guild.id,
-			user_id  = interaction.user.id
-		)
-		money_unit = await self.donation_manager.get_money_unit(
-			guild_id = interaction.guild.id
+			guild_id  = interaction.guild.id,
+			user_id   = interaction.user.id,
+			unit_name = unit_name
 		)
 
 		await interaction.followup.send(embed = Embed(
 			title = "Donation Stats",
 			description = (
-				f"* You have now donated `{Number.fmt_donation(money)}` `{money_unit}` to the "
+				f"* You have now donated `{Number.fmt_donation(money)}` {unit_name} to the "
 				f"{interaction.guild.name}"
 			),
 			color     = 0xdfebbc,
